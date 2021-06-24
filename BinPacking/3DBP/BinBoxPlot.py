f361c19832b95d3f2c457b5468435bb5ba4589b7
YangKai <85356532+YangKai119@users.noreply.github.com>
2021-06-24 21:29:43 +0800

    Update BinBoxPlot.py

--- BinPacking/3DBP/BinBoxPlot.py
+++ BinPacking/3DBP/BinBoxPlot.py
@@ -3,7 +3,7 @@ from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-#make_pic的内置函数，用来在图像里面不断添加立方体
+# 用来在图像里面不断添加立方体
 def box(ax,x, y, z, dx, dy, dz, color='red'):
     xx = [x, x, x+dx, x+dx, x]
     yy = [y, y+dy, y+dy, y, y]
@@ -15,8 +15,8 @@ def box(ax,x, y, z, dx, dy, dz, color='red'):
     ax.plot3D([x+dx, x+dx], [y+dy, y+dy], [z, z+dz], **kwargs)
     ax.plot3D([x+dx, x+dx], [y, y], [z, z+dz], **kwargs)
     return ax
-#显示图形的函数：Items = [[num[0],num[1],num[2],num[3],num[4],num[5],num[6]],]
-#Items是N个列表的列表，里面的每个列表数据[放置点O三维坐标，长宽高，颜色]
+# 显示图形的函数：Items = [[num[0],num[1],num[2],num[3],num[4],num[5],num[6]],]
+# Items是N个列表的列表，里面的每个列表数据[放置点O三维坐标，长宽高，颜色]
 def show_pic(Items):
     fig = plt.figure()
     ax = Axes3D(fig)
@@ -27,7 +27,7 @@ def show_pic(Items):
         box(ax,num[0],num[1],num[2],num[3],num[4],num[5],num[6])
     plt.title('Cube')
     plt.show()
-#根据图显需要的数据，把尺寸数据生成绘图数据的函数
+# 根据图显需要的数据，把尺寸数据生成绘图数据的函数
 def packaging(O,C,color):
     data = [O[0],O[1],O[2],C[0],C[1],C[2],color]
     return data
